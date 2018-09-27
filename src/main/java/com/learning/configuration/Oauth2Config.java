c5ea552a946044255b16f1bd3c8205d1e06ccee8
Nidish <nidhishkrishnan2009@gmail.com>
2018-09-27 09:53:30 +0530

    config files

--- src/main/java/com/learning/configuration/Oauth2Config.java
+++ src/main/java/com/learning/configuration/Oauth2Config.java
@@ -1,59 +1,49 @@
 package com.learning.configuration;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
-@PropertySource({"classpath:persistence.properties"})
 public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
-	@Qualifier("authenticationManagerBean")
 	private AuthenticationManager authenticationManager;
-	
-	@Autowired
-	private Environment env;
-	
+
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer oauthServer) 
 	{
 		oauthServer
-			.tokenKeyAccess("permitAll")
+			.tokenKeyAccess(" permitAll")
 			.checkTokenAccess("isAuthenticated()");
 	}
 	
-//	@Override
-//	public void configure(ClientDetailsServiceConfigurer clients) 
-//	{
-//		try {
-//			clients
-//				.jdbc(dataSource());
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-	
-	@Bean
-	public DataSource dataSource() {
-	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	    dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-	    dataSource.setUrl(env.getProperty("jdbc.url"));
-	    dataSource.setUsername(env.getProperty("jdbc.user"));
-	    dataSource.setPassword(env.getProperty("jdbc.pass"));
-	    return dataSource;
+		
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+				.withClient("ClientId")
+				.secret("secret")
+				.authorizedGrantTypes("authorization_code")
+				.scopes("user_info")
+				.autoApprove(true);
 	}
+
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
+	}
+
+
+	
+
 }
